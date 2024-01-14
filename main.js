1f8d0419da0682642b4cb0ce246b4c9c6b05d2f2
Isabella Cristina <137854113+isa-cristina@users.noreply.github.com>
2024-01-14 17:30:15 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,90 @@
+/* 1. Crie um script que exiba a mensagem "Hello World!" em um alerta no navegador.*/
+// alert("Hello world!");
+
+
+/* 2. Crie um script que declare duas variáveis e exiba o resultado da soma entre elas.*/
+// let N1 = 2,
+// N2 = 3;
+// alert(N1+=N2)
+
+
+/* 3. Crie um script que declare uma variável e verifique se o seu valor é um número.
+Se for, exiba a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número".*/
+// let x = 8;
+// if (typeof x == "number") {
+//   console.log("É um número.");
+// } else {
+//   console.log("Não é um número!");
+// }
+
+//Ou posso usar isNaN(), também servirá.
+//Esse "number" ou "string" funcionam SOMENTE com a função typeof, porque é um tipo de dado 
+//acessível somente por essa palavra reservada.
+
+
+/*4. Crie um script que declare uma variável e verifique se o seu valor é uma string.
+Se for, exiba a mensagem "É uma string", caso contrário, exiba a mensagem "Não é uma string".*/
+
+// let x = 8;
+
+// if (typeof x == "string") {
+//   console.log("É uma string!");
+// } else {
+//   console.log("Não é uma string!");
+// }
+
+
+//5. Crie um script que declare uma variável e verifique se o seu valor é um booleano. 
+//Se for, exiba a mensagem "É um booleano", caso contrário, exiba a mensagem "Não é um booleano".
+
+// let x = true;
+
+// if (typeof x == "boolean") {
+//   console.log("É um boolean!");
+// } else {
+//   console.log("Não é um boolean!");
+// }
+
+
+//6. Crie um script que declare duas variáveis e exiba o resultado da subtração entre elas.
+
+// let firstNum = 18;
+// let secondNum = 15;
+// let sub = (x,y) => x-y;
+// console.log(sub(firstNum,secondNum));
+
+//Arrow function subtitui um "ponteiro".
+
+
+//7. Crie um script que declare duas variáveis e exiba o resultado da multiplicação entre elas.
+// let firstNum = 18;
+// let secondNum = 15;
+// let sub = (x,y) => x*y;
+// console.log(sub(firstNum,secondNum));
+
+
+//8. Crie um script que declare duas variáveis e exiba o resultado da divisão entre elas.
+// let firstNum = 18;
+// let secondNum = 15;
+// let sub = (x,y) => x/y;
+// console.log(sub(firstNum,secondNum));
+
+
+//9. Crie um script que declare uma variável e verifique se o seu valor é um número par. 
+//Se for, exiba a mensagem "É um número par", caso contrário, exiba a mensagem "Não é um número par".
+// let num = 33;
+// if(num%2 == 0){
+//     console.log("É um número par!")
+// }else{
+//     console.log("Não é um número par.")
+// }
+
+
+//10. Crie um script que declare uma variável e verifique se o seu valor é um número ímpar. 
+// Se for, exiba a mensagem "É um número ímpar", caso contrário, exiba a mensagem "Não é um número ímpa
+// let num = 33;
+// if(num%2 != 0){
+//     console.log("É um número ímpar!")
+// }else{
+//     console.log("Não é um número ímpar.")
+// }
